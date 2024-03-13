95a6bf2b8ad1a1428992cdf9f2842fe9234a6e10
shatakd00 <163285253+shatakd00@users.noreply.github.com>
2024-03-13 15:27:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Main from './components/file/main/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <Main/>
+}
+
+export default App;
